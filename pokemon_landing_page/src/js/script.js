e81bc92d2af312187c1929c1908898f831b29174
Eduardo Henrique <eduardos8236@gmail.com>
2025-01-18 22:37:57 -0300

    pk_dlt

--- pokemon_landing_page/src/js/script.js
+++
@@ -1,7 +0,0 @@
-import { load_pokemons } from "./pokecards.js"
-import { trocar_tema } from "./themes.js"
-
-const theme_button = document.querySelector("button#theme-button")
-
-theme_button.addEventListener("click", trocar_tema)
-document.addEventListener("DOMContentLoaded", load_pokemons)
